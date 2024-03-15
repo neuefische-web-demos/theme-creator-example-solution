465944b3cd539f9c8c9c9319e574c37f9c9818d9
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-03-15 14:57:35 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <h1>Theme Creator</h1>
+      </header>
+      <main className="main-container">TODO: Add Theme</main>
+    </>
+  );
+}
+
+export default App;
